6dd44fb8af61ee02480fd4c7b5c88b198d547145
jesser pariona <jerspr@gmail.com>
2016-08-02 22:43:49 -0500

    üpdate

---
+++ puntos/movil/js/loader.js
@@ -0,0 +1,20 @@
+/**
+ * Created by luisvar on 12/12/15.
+ */
+
+    $(document).ready(function(){
+        obj.slidePromocion();
+        obj.slideMarcas();
+        obj.menu_lateral();
+        obj.search_show();
+        obj.search_show_ciudad();
+        ////////////////////////////
+        obj.modalCenter();
+        obj.validateEmailCupon();
+        obj.checkActive();
+        obj.validateRespuesta();
+        /*Puntos*/
+        obj.flyoutMovil();
+        obj.menu_user();
+        obj.enviarPuntos();
+    });
